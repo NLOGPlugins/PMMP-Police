7399003ac90979cd924a2821df038fb5e12c8ed1
NLOG <sorisem4106@naver.com>
2017-06-21 22:20:13 +0900

    custom_cmd 구현완료
    2.0.0-beta-1 출시

--- src/nlog/Police/Main.php
+++ src/nlog/Police/Main.php
@@ -11,6 +11,7 @@ use pocketmine\command\Command;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\utils\Utils;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
 
 class Main extends PluginBase implements Listener{
 	
@@ -18,21 +19,29 @@ class Main extends PluginBase implements Listener{
 	 	$host = json_decode(Utils::getURL("https://raw.githubusercontent.com/ParkChanSol/PMMP-Police/master/" . $this->getDescription()->getVersion() ), true);
 	 	$new = json_decode(Utils::getURL("https://raw.githubusercontent.com/ParkChanSol/PMMP-Police/master/current"), true);
 	 	
-	 	if((!(isset($host["status"]))) or ($host["status"] !== true)) {
-	 		$this->getLogger()->alert("업데이트 호스트에 문제가 있어 확인할 수 없습니다.");
+	 	$this->getLogger()->notice("업데이트 확인 중...");
+	 	if(!(isset($new["status"]))) {
+	 		$this->getLogger()->alert("업데이트 호스트에 문제가 있어 확인할 수 없습니다. >> 플러그인을 비활성화합니다.");
+	 		$this->getPluginLoader()->disablePlugin($this);
+			return;
+	 	}
+		
+		if(!(isset($new["status"]))) {
+	 		$this->getLogger()->alert("업데이트 호스트에 문제가 있어 확인할 수 없습니다. >> 플러그인을 비활성화합니다.");
+	 		$this->getPluginLoader()->disablePlugin($this);
 			return;
 	 	}
 	 	
-	 	if ($host["beta-available"] === true) {
+	 	if ($new["beta-available"] === true) {
 	 		$this->getLogger()->notice("베타 버전을 사용할 수 있습니다.");
 			$this->getLogger()->notice("개발자 블로그에서 이용하실 수 있습니다.");
-	 		$this->getLogger()->notice("베타는" . $host["beta"] . "버전입니다.");
+	 		$this->getLogger()->notice("베타는" . $new["beta"] . "버전입니다.");
 			return;
 	 	}
 	 	
-	 	if ($host["update-available"] === true) {
+	 	if (!($this->getDescription()->getVersion() === $new["ver"])) {
 	 		$this->getLogger()->notice($new["ver"] . "이 출시되었습니다.");
-			$this->getLogger()->notice("개발자 블로그에서 새로운 버전을 다운해주세요.");
+			$this->getLogger()->notice("개발자 블로그에서 새로운 버전을 다운해주세요.  >> 플러그인을 비활성화합니다.");
 	 		$this->getLogger()->notice($host["notice"]);
 	 		$this->getPluginLoader()->disablePlugin($this);
 			return;
@@ -41,6 +50,10 @@ class Main extends PluginBase implements Listener{
 	 	if ($this->getDescription()->getVersion() === $new["ver"]) {
 	 		$this->getLogger()->notice("최신 버전입니다.");
 			return;
+	 	}else{
+	 		$this->getLogger()->critical("알 수 없는 오류입니다.  >> 플러그인을 비활성화합니다.");
+	 		$this->getPluginLoader()->disablePlugin($this);
+	 		return;
 	 	}
 	 	
 	 	
@@ -96,30 +109,29 @@ class Main extends PluginBase implements Listener{
  	 }
  	 
  	 
- 	 
  	 //커맨드 API
- 	 public function getCmd($cmd) {
+ 	 public function getCmd() {
  	 	/*
  	 	 * 커맨드를 Config에서 가져옵니다.
  	 	 */
  	 	return $this->cmd->getAll(true);
  	 }
  	 
- 	 public function isCmd($cmd) {
+ 	 public function isCmd($command) {
  	 	/*
  	 	 * 커맨드가 잇으면 true, 없으면 false를 반환합니다.
  	 	 */
- 	 	return $this->cmd->exists($cmd);
+ 	 	return $this->cmd->exists($command);
  	 }
  	 
- 	 public function setCmd($permission) {
- 	 	$this->cmd->set($permission, "permission");
+ 	 public function setCmd($command) {
+ 	 	$this->cmd->set($command, "command");
  	 	$this->cmd->save();
  	 	return true;
  	 }
  	 
- 	 public function removeCmd($permission) {
- 	 	$this->cmd->remove($permission, "permission");
+ 	 public function removeCmd($command) {
+ 	 	$this->cmd->remove($command, "command");
  	 	$this->cmd->save();
  	 }
  	 
@@ -178,6 +190,9 @@ class Main extends PluginBase implements Listener{
 			}
  	 	}
  	 	
+ 	 	
+ 	 	
+ 	 	
  	 	#-----------------------------------------------------------------------------
  	 	#-----------------------------------------------------------------------------
  	 	$msg = "§b§o [ 알림 ] §7/policecmd <add | remove> <command> \n §b§o[ 알림 ] §7/policecmd list";
@@ -196,16 +211,14 @@ class Main extends PluginBase implements Listener{
  	 			if (!(isset($args[1]))) {
  	 				$sender->sendMessage($msg);
  	 				return true;
- 	 			} //닉네임이 없을 때
+ 	 			} //커맨드가 입력하지 않았을 때
  	 			
  	 			if ($this->getServer()->getCommandMap()->getCommand(strtolower($args[1])) === null) {
  	 				$sender->sendMessage("§b§o [ 알림 ] §7명령어가 존재하지 않습니다.");
  	 				return true;
- 	 			}
- 	 			
- 	 			$permission = $this->getServer()->getCommandMap()->getCommand(strtolower($args[1]))->getPermission();
+ 	 			} //커맨드가 존재하지 않을 때
  	 	
- 	 			$this->setCmd($permission);
+ 	 			$this->setCmd(strtolower($args[1]));
  	 			$sender->sendMessage("§b§o [ 알림 ] §7명령어 '".strtolower($args[1])."'는 경찰이 사용할 수 있습니다.");
  	 			return true;
  	 		}
@@ -215,31 +228,37 @@ class Main extends PluginBase implements Listener{
  	 				$sender->sendMessage($msg);
  	 				return true;
  	 			} //닉네임이 없을 때
+ 	 			
+ 	 			if ($this->getServer()->getCommandMap()->getCommand(strtolower($args[1])) === null) {
+ 	 				$sender->sendMessage("§b§o [ 알림 ] §7명령어가 존재하지 않습니다.");
+ 	 				return true;
+ 	 			}
  	 	
  	 			if (!($this->isCmd(strtolower($args[1])))) {
- 	 				$sender->sendMessage("§b§o [ 알림 ] §7이 플레이어는 경찰이 아닙니다.");
+ 	 				$sender->sendMessage("§b§o [ 알림 ] §7이 명령어는 등록되어 있지 않습니다.");
  	 				return true;
  	 			} //닉네임이 경찰이 아닐 때
- 	 	
- 	 			$this->removePolice($args[1]);
- 	 			$sender->sendMessage("§b§o [ 알림 ] §7".$args[1]."님을 경찰에서 제거하였습니다.");
+ 	 			
+ 	 			$this->removeCmd(strtolower($args[1]));
+ 	 			$sender->sendMessage("§b§o [ 알림 ] §7 명령어 '".$args[1]."'는 더이상 경찰이 사용할 수 없습니다");
  	 			return true;
  	 		}
  	 		#-----------------------------------------------------------------------------
  	 		if ($args[0] === "list") {
- 	 			$list = implode(", ", $this->getPolice());
- 	 			$sender->sendMessage("§b§o [ 알림 ] §7경찰 목록 : " . $list);
+ 	 			$list = implode(", ", $this->getCmd());
+ 	 			$sender->sendMessage("§b§o [ 알림 ] §7경찰이 사용할 수 있는 명령어 목록 : " . $list);
  	 			return true; //리스트
  	 			#-----------------------------------------------------------------------------
  	 		}else{
  	 			$sender->sendMessage($msg);
- 	 			return true; //$args[0]이 없을 때
+ 	 			return true; //$args[0]가 존재하지 않을 때
  	 			#-----------------------------------------------------------------------------
  	 		}
  	 	}
  	 	
  	 }
  	 
+ 	 /*
  	 public function onJoin (PlayerJoinEvent $ev) {
 			$name = $ev->getPlayer()->getName();
 			if ($this->isPolice(strtolower($name)) === true) {
@@ -258,6 +277,20 @@ class Main extends PluginBase implements Listener{
  	 	$per->setPermission("pocketmine.command.teleport", false);
  	 	$per->setPermission("pocketmine.command.list", false);
  	 	$per->setPermission("solobanmaster.command.police", false);
+ 	 }*/
+ 	 
+ 	 public function onCommandProcessEvent(PlayerCommandPreprocessEvent $ev) {
+ 	 	$player = $ev->getPlayer();
+ 	 	$msg = $ev->getMessage();
+ 	 	$words = explode(" ", $msg);
+ 	 	$cmd = strtolower(substr(array_shift($words), 1));
+ 	 	if ($this->isCmd($cmd)) {
+ 	 		if ($this->isPolice(strtolower($player->getName()))) {
+ 	 			$ev->setCancelled(true);
+ 	 			$this->getServer()->broadcastMessage("§b§o [ 알림 ] §7경찰 " . $player->getName() . "님이" . $cmd . "명령어를 사용하였습니다.");
+ 	 			$this->getServer()->dispatchCommand($sender, $msg);
+ 	 		}
+ 	 	}
  	 }
   }
 ?>
